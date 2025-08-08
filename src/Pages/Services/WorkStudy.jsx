1519a4592647106de78fb6b5980d137fd538f667
adr-2 <www.adriensasni@gmail.com>
2025-08-09 00:55:59 +0100

    fix:disposition de la div team et div avantages

--- src/Pages/Services/WorkStudy.jsx
+++ src/Pages/Services/WorkStudy.jsx
@@ -20,109 +20,127 @@ export function WorkStudy() {
         <WorkAccordionMenu />
       </div>
 
-      {/* section header */}
-      <section className="bg-blue-900 text-white  py-20 px-10 md:px-20">
-        <div className=" md:w-6/10">
-          <h1 className="text-3xl font-bold mb-4">
-            Pour ce projet, notre TEAM vous garantie
-          </h1>
-          <ul className="list-disc font-semibold mt-6 text-justify">
-            <li>Une écoute attentive de vos besoins. </li>
-            <li>
-              Une Orientation gratuite en ce qui concerne vos choix académiques,
-              ainsi qu’un accompagnement facilitant d’éventuel besoin de
-              reconversion ou de réorientation vers de nouveaux horizons.
-            </li>
-            <li>
-              Le conseil quant au choix de votre ville de destination le cas
-              échéant (coût de la vie, aspect cosmopolite de la ville,
-              opportunités offertes dans la ville etc...)
-            </li>
-            <li>
-              Une ou plusieurs séances de Coaching en fonction du besoin du
-              candidat. Nous travaillons avec des Coachs Camerounais certifiés
-              par des Associations de Coaching reconnues mondialement. Nous
-              apportons au candidat un accompagnement exclusif afin de faire
-              ressortir ses forces et lui donner confiance en lui pour affronter
-              l’officier consulaire au jour de l’entretiens de visa.
-            </li>
-            <li>
-              Un réseau de contacts qui facilitent la procédure de l’inscription
-              à l’hébergement en passant par la certification et la traduction
-              documents, le visa, le billet d’avion…
-            </li>
-            <li>
-              Un réseau d’anciens étudiants ayant bénéficié de nos services et
-              avec qui nous travaillons, qui vous accueillent à l’aéroport de
-              votre pays de destination, vous logent, vous aide à vous
-              socialiser (ouverture de compte bancaire, choix d’un abonnement
-              téléphonique, premier shopping, la culture du milieu…) vous
-              accompagnent à l’école le premier jour, vous conseillent et
-              facilitent votre intégration dans votre nouvel environnement.
-            </li>
-            <li>
-              La mise en relation avec le service des Etudiants Internationaux
-              de l’université choisie à toutes fins utiles.
-            </li>
-          </ul>
-          <p>
-            <span>NB</span>:
-          </p>
-        </div>
-        <div>
-          <h3 className="text-xl font-semibold mt-6">
-            Pourquoi choisir le travail-études ?
-          </h3>
-          <ul className="list-decimal pl-5 mt-2">
-            <li>Expérience professionnelle précieuse</li>
-            <li>Flexibilité pour concilier études et travail</li>
-            <li>Opportunités de réseautage</li>
-            <li>Amélioration des compétences pratiques</li>
-            <li>Flexibilité pour concilier études et travail</li>
-            <li>Opportunités de réseautage</li>
-            <li>Amélioration des compétences pratiques</li>
-          </ul>
-          <p>
-            <span>NB</span>:Pour avoir d’ample information sur la légalité du
-            programme de Work & Study,{" "}
-            <a href="https://www.ice.gov/sevis/practical-training">
-              cliquez ICI{" "}
-            </a>{" "}
-            pour être redirigé vers le site du gouvernement Américain traitant
-            d’immigration et de douane.
-          </p>
-        </div>
-      </section>
-
       {/* section 1 */}
-      <section className="max-w-6xl mx-auto p-6 bg-white shadow-md mt-10">
-        <div>
-          <h2 className="text-2xl font-semibold mb-4">Nos Services</h2>
-          <p>
-            Nous offrons des conseils et un accompagnement pour trouver des
-            programmes de travail-études adaptés à vos besoins académiques et
-            professionnels.
-          </p>
+      <section className="bg-[#D5DCF5]  py-20 px-10 md:px-20 flex flex-col justify-center sm:justify-normal sm:flex-row gap-10">
+        <div className="flex flex-col sm:px-20 ">
+          <div className=" ">
+            <h1 className="text-3xl font-bold  mb-4 text-blue-950">
+              Pour ce projet, notre TEAM vous garantie
+            </h1>
+            <p className="border-b-2 border-b-blue-900 sm:w-140 h-2 "></p>
+            <ul className="list-disc font-semibold mt-6 text-justify flex flex-col gap-4">
+              <li>Une écoute attentive de vos besoins. </li>
+              <li>
+                Une Orientation gratuite en ce qui concerne vos choix
+                académiques, ainsi qu’un accompagnement facilitant d’éventuel
+                besoin de reconversion ou de réorientation vers de nouveaux
+                horizons.
+              </li>
+              <li>
+                Le conseil quant au choix de votre ville de destination le cas
+                échéant (coût de la vie, aspect cosmopolite de la ville,
+                opportunités offertes dans la ville etc...)
+              </li>
+              <li>
+                Une ou plusieurs séances de Coaching en fonction du besoin du
+                candidat. Nous travaillons avec des Coachs Camerounais certifiés
+                par des Associations de Coaching reconnues mondialement. Nous
+                apportons au candidat un accompagnement exclusif afin de faire
+                ressortir ses forces et lui donner confiance en lui pour
+                affronter l’officier consulaire au jour de l’entretiens de visa.
+              </li>
+              <li>
+                Un réseau de contacts qui facilitent la procédure de
+                l’inscription à l’hébergement en passant par la certification et
+                la traduction documents, le visa, le billet d’avion…
+              </li>
+              <li>
+                Un réseau d’anciens étudiants ayant bénéficié de nos services et
+                avec qui nous travaillons, qui vous accueillent à l’aéroport de
+                votre pays de destination, vous logent, vous aide à vous
+                socialiser (ouverture de compte bancaire, choix d’un abonnement
+                téléphonique, premier shopping, la culture du milieu…) vous
+                accompagnent à l’école le premier jour, vous conseillent et
+                facilitent votre intégration dans votre nouvel environnement.
+              </li>
+              <li>
+                La mise en relation avec le service des Etudiants Internationaux
+                de l’université choisie à toutes fins utiles.
+              </li>
+            </ul>
+            <p>
+              <span className="font-bold">NB</span>: Pour avoir d’ample
+              information sur la légalité du programme de Work & Study,{" "}
+              <a
+                href="https://www.ice.gov/sevis/practical-training"
+                className="text-blue-600 underline hover:text-blue-900"
+              >
+                cliquez ICI
+              </a>{" "}
+              pour être redirigé vers le site du gouvernement Américain traitant
+              d’immigration et de douane.
+            </p>
+          </div>
+          <div className="bg-blue-900 shadow-md p-6 mt-10 rounded-lg w-80 sm:w-100 text-white flex flex-col justify-center items-center">
+            <h3 className="text-xl font-semibold mt-6 text-center">
+              Pourquoi choisir le travail-études ?
+            </h3>
+            <ul className="list-decimal pl-5 mt-2 border-l-2 border-white">
+              <li>Expérience professionnelle précieuse</li>
+              <li>Flexibilité pour concilier études et travail</li>
+              <li>Opportunités de réseautage</li>
+              <li>Amélioration des compétences pratiques</li>
+              <li>Flexibilité pour concilier études et travail</li>
+              <li>Opportunités de réseautage</li>
+              <li>Amélioration des compétences pratiques</li>
+            </ul>
+          </div>
+        </div>
+        {/*  */}
+        <div className=" flex flex-col gap-50 justify-center sm:justify-normal">
+          <div className="bg-white shadow-md mt-10 w-70 h-80"></div>
+          <div className=" bg-white shadow-md mt-10 w-70 h-80"></div>
         </div>
       </section>
 
       {/* section 2 */}
-      <section className="max-w-6xl mx-auto p-6 bg-white shadow-md mt-10">
-        <div>
-          <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-          </ul>
-        </div>
-        <div>
-          <h1></h1>
-          <p></p>
-          <p></p>
-          <p></p>
-        </div>
-      </section>
+      {/* <section className=" p-6 bg-white shadow-md mt-10 w-2/10">
+          <div className=" p-6 bg-white shadow-md mt-10 w-2/10">
+            <h2 className="text-2xl font-bold mb-4 text-blue-950">
+              Comment ça marche ?
+            </h2>
+            <p className="text-gray-700 mb-6">
+              Le programme de travail-études est conçu pour permettre aux
+              étudiants internationaux de travailler à temps partiel tout en
+              poursuivant leurs études. Voici comment cela fonctionne :
+            </p>
+            <ul className="list-disc pl-5 space-y-2">
+              <li>
+                <strong>Inscription :</strong> Les étudiants intéressés doivent
+                s'inscrire au programme de travail-études auprès de leur
+                établissement d'enseignement.
+              </li>
+              <li>
+                <strong>Recherche d'emploi :</strong> Les étudiants peuvent
+                rechercher des emplois sur le campus ou dans la communauté
+                locale qui sont compatibles avec leurs horaires d'études.
+              </li>
+              <li>
+                <strong>Heures de travail :</strong> Les étudiants peuvent
+                travailler jusqu'à 20 heures par semaine pendant les sessions
+                académiques et à temps plein pendant les vacances scolaires.
+              </li>
+              <li>
+                <strong>Rémunération :</strong> Les étudiants sont rémunérés
+                pour leur travail, ce qui peut les aider à couvrir une partie de
+                leurs frais de subsistance.
+              </li>
+            </ul>
+          </div>
+          <div></div>
+        </section> */}
     </div>
+    // </div>
   );
 }
 
