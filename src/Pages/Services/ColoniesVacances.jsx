ea1d65f94019d8d1ee92e38b1ff74d3ab1c510dd
adr-2 <www.adriensasni@gmail.com>
2025-08-13 19:00:44 +0100

    fix: des pages services

--- src/Pages/Services/ColoniesVacances.jsx
+++ src/Pages/Services/ColoniesVacances.jsx
@@ -13,7 +13,7 @@ export function ColoniesVacances() {
     <div>
       <Caroussel images={images} />
       {/*  */}
-      <section className="flex flex-col items-center p-25 gap-y-5">
+      <section className="flex flex-col items-center p-20 gap-y-5">
         <p className="text-justify text-lg pl-20 pr-20">
           Les échanges interculturels et les expériences internationales sont
           atouts inestimables pour l’éducation de nos enfants. Nous le savons,
@@ -21,19 +21,20 @@ export function ColoniesVacances() {
           le développement harmonieux de leurs enfants.
         </p>
         <p className="text-justify text-lg  pl-20 pr-20">
-          Advisers Agency et ses partenaires vous proposent des camps d’été
-          thématiques et non thématiques avec à la clé la création d'un réseau
-          d'amis venant du monde entier, qu'ils sauront valoriser dans le futur
-          proche. De plus, les participants interagissent avec d'autres pairs de
-          différents pays et apprennent les bases d'une langue seconde de leur
-          choix pendant les camp. Le leadership, le réseautage, le
-          divertissement, l’athlétisme et l'apprentissage sont les piliers qui
-          sous tendent les camps de vacances que nous vous proposons.
+          <span className="font-bold">Advisers Agency</span> et ses partenaires
+          vous proposent des camps d’été thématiques et non thématiques avec à
+          la clé la création d'un réseau d'amis venant du monde entier, qu'ils
+          sauront valoriser dans le futur proche. De plus, les participants
+          interagissent avec d'autres pairs de différents pays et apprennent les
+          bases d'une langue seconde de leur choix pendant les camp. Le
+          leadership, le réseautage, le divertissement, l’athlétisme et
+          l'apprentissage sont les piliers qui sous tendent les camps de
+          vacances que nous vous proposons.
         </p>
       </section>
       {/*  */}
       <section>
-        <div className="grid grid-cols-2 grid-rows-2 p-30 gap-0 justify-items-center text-white">
+        <div className="grid grid-cols-2 grid-rows-2 pl-30 pr-30 gap-0 justify-items-center text-white">
           {/* grid1 */}
           <div className="flex flex-row items-center justify-center ">
             <div className="w-64 h-94  bg-red-700 flex flex-col gap-y-20 p-4 justify-center items-center">
@@ -43,10 +44,12 @@ export function ColoniesVacances() {
                 Se plonger dans l'univers d'un métier et découvrir les aptitudes
                 requises pour l'effectuer
               </p>
-              <button>En savoir plus</button>
+              <button className="border-2 border-amber-500 hover:bg-amber-500 text-white rounded-lg p-2">
+                En savoir plus
+              </button>
             </div>
 
-            <div className="w-64 h-94">
+            <div className="w-64 h-94 transform-3d">
               <img src={`${grid1}`} alt="" />
             </div>
           </div>
@@ -59,9 +62,11 @@ export function ColoniesVacances() {
                 Apprenez les bases de votre sport préféré avec des coachs
                 qualifiés, le tout dans le FUN
               </p>
-              <button>En savoir plus</button>
+              <button className="border-2 border-green-500 hover:bg-green-500 text-white rounded-lg p-2">
+                En savoir plus
+              </button>
             </div>
-            <div className="w-64 h-94">
+            <div className="w-64 h-94  transform-3d">
               <img src={`${grid2}`} alt="" />
             </div>
           </div>
@@ -77,7 +82,9 @@ export function ColoniesVacances() {
                 choisissez le moment idéal pour vous amuser, nous vous assistons
                 avec nos conseils{" "}
               </p>
-              <button>En savoir plus</button>
+              <button className="border-2 border-blue-500 hover:bg-blue-500 text-white rounded-lg p-2">
+                En savoir plus
+              </button>
             </div>
           </div>
           {/* grid4 */}
@@ -92,14 +99,18 @@ export function ColoniesVacances() {
                 Suis-je éligible au colonies de vacances? Que faut il savoir au
                 sujet des{" "}
               </p>
-              <button>En savoir plus</button>
+              <button className="border-2 border-red-500 hover:bg-red-500 text-white rounded-lg p-2">
+                En savoir plus
+              </button>
             </div>
           </div>
         </div>
       </section>
       {/*  */}
-      <section className="bg-white p-16 border-2 border-gray-300">
-        <button>Nous contacter</button>
+      <section className="bg-white p-16 border-2 border-gray-300 flex  justify-end gap-x-10">
+        <button className="p-4 cursor-pointer  bg-blue-700 hover:bg-blue-600 text-white rounded-lg">
+          Nous contacter
+        </button>
       </section>
     </div>
   );
--- src/Pages/Services/ImmigrationCanadienne.jsx
+++ src/Pages/Services/ImmigrationCanadienne.jsx
@@ -4,8 +4,10 @@ function ImmigrationCanadienne() {
     <div>
       <img src={`${image1}`} alt="" />
       {/*  */}
-      <section className="bg-white p-16 border-2 border-gray-300">
-        <button>Nous contacter</button>
+      <section className="bg-white p-16 border-2 border-gray-300 flex  justify-end gap-x-10">
+        <button className="p-4 cursor-pointer  bg-blue-700 hover:bg-blue-600 text-white rounded-lg">
+          Nous contacter
+        </button>
       </section>
     </div>
   );
